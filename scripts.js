ae16663920495f61ed761d3dfae1f38007906067
xDVOLx <156858542+xDVOLx@users.noreply.github.com>
2024-03-03 19:26:09 -0800

    create function to play 5 games, tracking score
    created the playGame function to loop through 5 playRounds and track score to log in the console at the end of the 5 rounds.

--- scripts.js
+++ scripts.js
@@ -20,31 +20,53 @@ function getPlayerChoice() {
     }
 }
 
-let computerSelection = getComputerChoice()
-let playerSelection = getPlayerChoice()
+let playerScore = 0
+let computerScore = 0
 
 function playRound(playerSelection, computerSelection) {
+    console.log(`computer chose ${computerSelection}`)
+    console.log(`player chose ${playerSelection}`)
     if (playerSelection === "rock" && computerSelection === "rock") {
         return "Both sides chose Rock. Tie!"
     } else if (playerSelection === "rock" && computerSelection === "paper") {
+        computerScore++
         return "Paper covers Rock. Computer wins!"
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore++
         return "Rock smashes Scissors. Player wins!"
     } else if (playerSelection === "paper" && computerSelection === "paper") {
         return "Both sides chose Paper. Tie!"
     } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore++
         return "Paper covers Rock. Player wins!"
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        computerScore++
         return "Scissors cut Paper. Computer wins!"
     } else if (playerSelection === "scissors" && computerSelection === "scissors") {
         return "Both sides chose Scissors. Tie!"
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        computerScore++
         return "Rock smashes Scissors. Computer wins!"
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore++
         return "Scissors cut Paper. Player wins!"
     }
 }
 
-console.log(`computer chose ${computerSelection}`)
-console.log(`player chose ${playerSelection}`)
-console.log(playRound(playerSelection, computerSelection))
+function playGame(rounds) {
+    let round = 0;
+    function playRoundWithDelay() {
+        if (round < rounds) {
+            let playerSelection = getPlayerChoice();
+            let computerSelection = getComputerChoice();
+            console.log(playRound(playerSelection, computerSelection));
+            round++;
+            setTimeout(playRoundWithDelay, 1000);
+        } else {
+            console.log(`Final Score\n Player: ${playerScore}\n Computer ${computerScore}`)
+        }
+    }
+    playRoundWithDelay();
+}
+
+playGame(5);
