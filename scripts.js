f2a5da27bfd6290dbc1c7d985e0cc7c04b6de820
xDVOLx <156858542+xDVOLx@users.noreply.github.com>
2024-03-07 17:16:56 -0800

    created scoreboard area and function to update scores

--- scripts.js
+++ scripts.js
@@ -1,47 +1,61 @@
-//__________SCORES____________
-let playerScore = 0
-let computerScore = 0
+//__________SCOREKEEPING____________
+let playerWins = 0;
+let computerWins = 0;
+const roundResults = document.getElementById('resultsArea');
+
+const playerScore = document.createElement('p');
+playerScore.textContent = `Player Score: ${playerWins}`;
+roundResults.appendChild(playerScore);
+
+const computerScore = document.createElement('p');
+computerScore.textContent = `Computer Score: ${computerWins}`;
+roundResults.appendChild(computerScore);
+
+function updateScores() { //to be called on button press after playRound()
+    playerScore.textContent = `Player Score: ${playerWins}`;
+    computerScore.textContent = `Computer Score: ${computerWins}`;
+};
 
 //__________COMPUTER CHOICE____________
 function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 3) + 1;
     if (randomNum === 1) {
-        return "rock"
+        return "rock";
     } else if (randomNum === 2) {
-        return "paper"
+        return "paper";
     } else if (randomNum === 3) {
-        return "scissors"
-    }
-}
+        return "scissors";
+    };
+};
 
 //_________GAME LOGIC____________
 function playRound(playerSelection, computerSelection) {
-    console.log(`computer chose ${computerSelection}`)
-    console.log(`player chose ${playerSelection}`)
+    console.log(`computer chose ${computerSelection}`);
+    console.log(`player chose ${playerSelection}`);
     if (playerSelection === "rock" && computerSelection === "rock") {
-        return "Both sides chose Rock. Tie!"
+        return "Both sides chose Rock. Tie!";
     } else if (playerSelection === "rock" && computerSelection === "paper") {
-        computerScore++
-        return "Paper covers Rock. Computer wins!"
+        computerWins++;
+        return "Paper covers Rock. Computer wins!";
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        playerScore++
-        return "Rock smashes Scissors. Player wins!"
+        playerWins++;
+        return "Rock smashes Scissors. Player wins!";
     } else if (playerSelection === "paper" && computerSelection === "paper") {
-        return "Both sides chose Paper. Tie!"
+        return "Both sides chose Paper. Tie!";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        playerScore++
-        return "Paper covers Rock. Player wins!"
+        playerWins++;
+        return "Paper covers Rock. Player wins!";
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        computerScore++
-        return "Scissors cut Paper. Computer wins!"
+        computerWins++;
+        return "Scissors cut Paper. Computer wins!";
     } else if (playerSelection === "scissors" && computerSelection === "scissors") {
-        return "Both sides chose Scissors. Tie!"
+        return "Both sides chose Scissors. Tie!";
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        computerScore++
-        return "Rock smashes Scissors. Computer wins!"
+        computerWins++;
+        return "Rock smashes Scissors. Computer wins!";
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        playerScore++
-        return "Scissors cut Paper. Player wins!"
+        playerWins++;
+        return "Scissors cut Paper. Player wins!";
     };
 };
 
@@ -50,27 +64,30 @@ const playerChoices = document.getElementById('playerChoiceBtns');
 
 const rockButton = document.createElement('button');
 rockButton.textContent = "Rock";
-playerChoices.appendChild(rockButton);
 rockButton.addEventListener('click', () => {
     playerSelection = "rock";
     computerSelection = getComputerChoice();
     playRound(playerSelection, computerSelection);
+    updateScores();
 });
+playerChoices.appendChild(rockButton);
 
 const paperButton = document.createElement('button');
 paperButton.textContent = "Paper";
-playerChoices.appendChild(paperButton);
 paperButton.addEventListener('click', () => {
     playerSelection = "paper";
     computerSelection = getComputerChoice();
     playRound(playerSelection, computerSelection);
+    updateScores();
 });
+playerChoices.appendChild(paperButton);
 
 const scissorsButton = document.createElement('button');
 scissorsButton.textContent = "Scissors";
-playerChoices.appendChild(scissorsButton);
 scissorsButton.addEventListener('click', () => {
     playerSelection = "scissors";
     computerSelection = getComputerChoice();
     playRound(playerSelection, computerSelection);
+    updateScores();
 });
+playerChoices.appendChild(scissorsButton);
